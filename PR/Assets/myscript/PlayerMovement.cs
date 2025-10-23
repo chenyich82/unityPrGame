9f2dfded3aea2babdc5673c4c2eba610ade0bc3f
CHEN <ggsica0418@gmail.com>
2025-10-23 18:39:11 +0800

    設定角色腳本初始

---
+++ PR/Assets/myscript/PlayerMovement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    //設定角色屬性
+    public float moveSpeed = 5f;// 移動速度
+    public float runSpeed = 10f;//跑步速度（按 Shift 時）
+    public float jumpForce = 7f;//跳要力度（AddForce 的大小）
+
+    
+}
